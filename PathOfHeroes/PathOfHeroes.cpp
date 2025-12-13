ee4d500cf0369b4c24e34a1fe9690bc5175c659a
Wojtek <Wojtek@WZKOMP>
2025-12-13 22:18:40 +0100

    First test fight

--- PathOfHeroes/PathOfHeroes.cpp
+++ PathOfHeroes/PathOfHeroes.cpp
@@ -1,34 +1,25 @@
-// PathOfHeroes.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
+#include "Player.h"
+#include "Enemy.h"
 
 int main()
 {
-    std::cout << "Hello World czesc!\n";
+	Player player(100, 20);
+	Enemy enemy(60, 10);
 
-    int tablica[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	std::cout << "Health - Player: " << player.getHealth() << ", Enemy: " << enemy.getHealth() << std::endl;
 
-    for (int i = 0; i < 10; i++) {
-        std::cout << tablica[i] << std::endl;
-    }
+	std::cout << "The battle begins! Player attacks Enemy!" << std::endl;
+	player.attack(&enemy);
 
-    return 0;
+	std::cout << "Health - Enemy: " << enemy.getHealth() << std::endl;
+
+	std::cout << "\nEnemy attacks Player!\n";
+	enemy.attack(&player);
+
+	std::cout << "Health - Player: " << player.getHealth() << std::endl;
 
 
-
-
-
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
