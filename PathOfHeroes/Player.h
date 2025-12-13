4d52be571e97573cc8f41163ed2da785044ed1a4
Wojtek <Wojtek@WZKOMP>
2025-12-13 22:00:16 +0100

    Player basics

--- PathOfHeroes/Player.h
+++ PathOfHeroes/Player.h
@@ -1,1 +1,13 @@
 #pragma once
+#include "Character.h"
+
+class Player : public Character {
+public:
+	Player(int hp, int ap);
+	void attack(Character* target) override;
+	void takeDamage(int dmg) override;
+};
+
+
+
+
