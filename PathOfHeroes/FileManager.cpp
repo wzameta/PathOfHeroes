62836ac43c59f563ae07069b951dcd70d0bfccc4
Wojtek <Wojtek@WZKOMP>
2025-12-16 22:55:35 +0100

    Files basics

---
+++ PathOfHeroes/FileManager.cpp
@@ -0,0 +1,13 @@
+#include "FileManager.h"
+#include "fstream"
+
+std::vector<std::string> FileManager::loadEnemies(const std::string& path) {}
+void FileManager::saveReport(const std::string& path, const std::string& text) {}
+
+
+
+
+
+
+
+
--- PathOfHeroes/FileManager.h
+++ PathOfHeroes/FileManager.h
@@ -1,1 +1,14 @@
 #pragma once
+#include <string>
+#include <vector>
+
+class FileManager {
+public:
+	static std::vector<std::string> loadEnemies(const std::string& path);
+	static void saveReport(const std::string& path, const std::string& text);
+};
+
+
+
+
+
