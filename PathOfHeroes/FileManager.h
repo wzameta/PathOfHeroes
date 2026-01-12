3140f3ea19e6801488c8cdda9252b347d10963d4
Karniszeryk <erykzmuda2005@gmail.com>
2026-01-12 13:32:04 +0100

    Dodanie deklaracji metody savereport

--- PathOfHeroes/FileManager.h
+++ PathOfHeroes/FileManager.h
@@ -1,11 +1,10 @@
-#pragma once
-#include <string>
-#include <vector>
-
 class FileManager {
 public:
-	static std::vector<std::string> loadEnemies(const std::string& path);
-	static void saveReport(const std::string& path, const std::string& text);
+    static void saveReport(
+        const std::string& playerName,
+        int playerHP,
+        int enemiesDefeated
+    );
 };
 
 
